e04e7856e861e88f379a8b9334347c959924fbcc
Zenghui Shi <zshi@redhat.com>
2020-11-17 18:54:06 +0800

    Several fixes
    use the right image for ovn-kubernetes
    fix the cno image override manifest name
    fix the conditional check of kubeconfig

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -n "${KUBECONFIG}" ]]; then
+if [[ ! -n "${KUBECONFIG}" ]]; then
 	echo "error: KUBECONFIG must be set or '-k <kubeconfig>' must be given"
 	exit 1
 fi
@@ -9,7 +9,7 @@ fi
 oc patch --type=json -p "$(cat manifests/override-cno-control-patch.yaml)" clusterversion version
 
 # overrides network operator deployment image
-oc patch -p "$(cat manifests/override-cno-deployment-image-patch.yaml)" deploy network-operator -n openshift-network-operator
+oc patch -p "$(cat manifests/override-cno-image-patch.yaml)" deploy network-operator -n openshift-network-operator
 
 # overrides ovn-kubernetes image
 oc patch -p "$(cat manifests/override-ovn-kubernetes-image-patch.yaml)" deploy network-operator -n openshift-network-operator
