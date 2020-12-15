6aa694420a4876b79af49f9b35b469dd51e44506
Zenghui Shi <zshi@redhat.com>
2020-12-15 10:56:22 +0800

    update ovn_node.sh and ovn_controller.sh scripts

--- manifests/bf_ovn_controller.sh
+++ manifests/bf_ovn_controller.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export K8S_NODE="worker-1"
+export K8S_NODE="worker-h09"
 export OVN_LOG_LEVEL="info"
+
+mkdir -p /var/run/ovn
+mkdir -p /var/lib/openvswitch/etc
+mkdir -p /var/lib/openvswitch/data
+mkdir -p /var/run/ovn
+mkdir -p /root/ovn-ca
+mkdir -p /root/ovn-cert
+mkdir -p /root/ovnkube-config
 
 podman run --pid host --network host --user 0 --name ovn-controller -dit --privileged \
 	-v /var/run/openvswitch:/run/openvswitch \
@@ -13,6 +21,7 @@ podman run --pid host --network host --user 0 --name ovn-controller -dit --privi
 	-v /root/ovn-ca:/ovn-ca \
 	-v /root/ovn-cert:/ovn-cert \
 	-e K8S_NODE=$K8S_NODE \
-	--entrypoint="ovn-controller unix:/var/run/openvswitch/db.sock -vfile:off --no-chdir --pidfile=/var/run/ovn/ovn-controller.pid -p /ovn-cert/tls.key -c /ovn-cert/tls.crt -C /ovn-ca/ca-bundle.crt -vconsole:$OVN_LOG_LEVEL -C /ovn-ca/ca-bundle.crt -p /ovn-cert/tls.key -c /ovn-cert/tls.crt" \
+	-e OVN_LOG_LEVEL=$OVN_LOG_LEVEL \
+	--entrypoint=/usr/bin/ovn-controller \
 	quay.io/zshi/ovn-daemonset:bluefield-arm \
-	"ovn-controller"
+	unix:/var/run/openvswitch/db.sock -vfile:off --no-chdir --pidfile=/var/run/ovn/ovn-controller.pid -p /ovn-cert/tls.key -c /ovn-cert/tls.crt -C /ovn-ca/ca-bundle.crt -vconsole:info
--- manifests/bf_ovn_node.sh
+++ manifests/bf_ovn_node.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-export K8S_NODE="worker-1"
-export OVN_KUBE_LOG_LEVEL=4
+export K8S_NODE="worker-h09"
+export OVN_KUBE_LOG_LEVEL=5
 export OVN_CONTROLLER_INACTIVITY_PROBE=30000
-export KUBERNETES_SERVICE_HOST="api-int.sriov.ovn.testing"
-export KUBERNETES_SERVICE_PORT="6443"
+export KUBERNETES_SERVICE_HOST=api-int.sriov.ovn.testing
+export KUBERNETES_SERVICE_PORT=6443
 export OVN_GATEWAY_OPTIONS="--gateway-mode local --gateway-interface br-ex"
 export OVN_NB_DB_LIST="ssl:192.168.111.20:9641,ssl:192.168.111.21:9641,ssl:192.168.111.22:9641"
 export OVN_SB_DB_LIST="ssl:192.168.111.20:9642,ssl:192.168.111.21:9642,ssl:192.168.111.22:9642"
 export GENEVE_PORT=6081
+export SMART_NIC_IP="192.168.111.55"
+
+
+mkdir -p /run/ovn-kubernetes
+mkdir -p /run/netns
+mkdir -p /var/lib/cni/bin
+mkdir -p /var/lib/cni/networks
+
+mkdir -p /var/run/secrets/kubernetes.io/serviceaccount
+mkdir -p /root/ovn-ca
+mkdir -p /root/ovn-cert
+mkdir -p /root/ovnkube-node
+touch /var/lib/cni/networks/ovn-k8s-cni-overlay
 
 podman run --pid host --network host --user 0 --name ovnkube-node -dit --privileged \
 	-v /etc/systemd/system:/etc/systemd/system \
@@ -24,11 +37,16 @@ podman run --pid host --network host --user 0 --name ovnkube-node -dit --privile
 	-v /root/ovnkube-config:/run/ovnkube-config \
 	-v /root/ovn-ca:/ovn-ca \
 	-v /root/ovn-cert:/ovn-cert \
+	-v /var/run/ovn:/var/run/ovn \
+	-v /var/run/secrets/kubernetes.io/serviceaccount:/var/run/secrets/kubernetes.io/serviceaccount \
 	-e K8S_NODE=$K8S_NODE \
 	-e OVN_KUBE_LOG_LEVEL=$OVN_KUBE_LOG_LEVEL \
 	-e OVN_CONTROLLER_INACTIVITY_PROBE=$OVN_CONTROLLER_INACTIVITY_PROBE \
 	-e KUBERNETES_SERVICE_HOST=$KUBERNETES_SERVICE_HOST \
 	-e KUBERNETES_SERVICE_PORT=$KUBERNETES_SERVICE_PORT \
-	--entrypoint="iptables -t raw -A PREROUTING -p udp --dport $GENEVE_PORT -j NOTRACK; iptables -t raw -A OUTPUT -p udp --dport $GENEVE_PORT -j NOTRACK; /usr/bin/ovnkube --init-node ${K8S_NODE} --nb-address $OVN_NB_DB_LIST --sb-address $OVN_SB_DB_LIST --nb-client-privkey /ovn-cert/tls.key --nb-client-cert /ovn-cert/tls.crt --nb-client-cacert /ovn-ca/ca-bundle.crt --nb-cert-common-name ovn --sb-client-privkey /ovn-cert/tls.key --sb-client-cert /ovn-cert/tls.crt --sb-client-cacert /ovn-ca/ca-bundle.crt --sb-cert-common-name ovn --config-file=/run/ovnkube-config/ovnkube.conf --loglevel ${OVN_KUBE_LOG_LEVEL}  --inactivity-probe=$OVN_CONTROLLER_INACTIVITY_PROBE $OVN_GATEWAY_OPTIONS --metrics-bind-address \"127.0.0.1:29103\""
-	quay.io/zshi/ovn-daemonset:bluefield-arm \
-	"ovnkube-node"
+	-e OVN_GATEWAY_OPTIONS=$OVN_GATEWAY_OPTIONS \
+	-e OVN_NB_DB_LIST=$OVN_NB_DB_LIST \
+	-e OVN_SB_DB_LIST=$OVN_SB_DB_LIST \
+	--entrypoint /usr/bin/ovnkube \
+	quay.io/zshi/ovn-daemonset:bluefield-arm-20201214 \
+	--init-node ${K8S_NODE} --encap-ip ${SMART_NIC_IP} --smart-nic --nb-address $OVN_NB_DB_LIST --sb-address $OVN_SB_DB_LIST --nb-client-privkey /ovn-cert/tls.key --nb-client-cert /ovn-cert/tls.crt --nb-client-cacert /ovn-ca/ca-bundle.crt --nb-cert-common-name ovn --sb-client-privkey /ovn-cert/tls.key --sb-client-cert /ovn-cert/tls.crt --sb-client-cacert /ovn-ca/ca-bundle.crt --sb-cert-common-name ovn --config-file=/run/ovnkube-config/ovnkube.conf --loglevel ${OVN_KUBE_LOG_LEVEL}  --inactivity-probe=$OVN_CONTROLLER_INACTIVITY_PROBE $OVN_GATEWAY_OPTIONS --metrics-bind-address 127.0.0.1:29103
