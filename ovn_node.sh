84e73230c84e28627bbe65b37b8dde1f0fa84a96
Zenghui Shi <zshi@redhat.com>
2021-04-02 11:47:08 +0800

    phase-2: support ovnkube node mgmt port

--- ovn_node.sh
+++ ovn_node.sh
@@ -7,6 +7,7 @@ export KUBERNETES_SERVICE_HOST=api-int.sriov.ovn.testing
 export KUBERNETES_SERVICE_PORT=6443
 export OVN_GATEWAY_OPTIONS="--gateway-mode local --gateway-interface br-ex"
 export OVNKUBE_NODE_MODE="--ovnkube-node-mode smart-nic"
+export OVNKUBE_NODE_MGMT_PORT_NETDEV="--ovnkube-node-mgmt-port-netdev eth3" # eth3 (ovn mgmt port on BF)
 export OVN_NB_DB_LIST="ssl:192.168.111.20:9641,ssl:192.168.111.21:9641,ssl:192.168.111.22:9641"
 export OVN_SB_DB_LIST="ssl:192.168.111.20:9642,ssl:192.168.111.21:9642,ssl:192.168.111.22:9642"
 export GENEVE_PORT=6081
@@ -63,4 +64,4 @@ podman run --pid host --network host --user 0 --name ovnkube-node -dit --privile
 	-e OVN_SB_DB_LIST=$OVN_SB_DB_LIST \
 	--entrypoint /usr/bin/ovnkube \
 	$OVN_K8S_IMAGE \
-	--init-node ${K8S_NODE} --encap-ip ${SMART_NIC_IP} --nb-address $OVN_NB_DB_LIST --sb-address $OVN_SB_DB_LIST --nb-client-privkey /ovn-cert/tls.key --nb-client-cert /ovn-cert/tls.crt --nb-client-cacert /ovn-ca/ca-bundle.crt --nb-cert-common-name ovn --sb-client-privkey /ovn-cert/tls.key --sb-client-cert /ovn-cert/tls.crt --sb-client-cacert /ovn-ca/ca-bundle.crt --sb-cert-common-name ovn --config-file=/run/ovnkube-config/ovnkube.conf --loglevel ${OVN_KUBE_LOG_LEVEL}  --inactivity-probe=$OVN_CONTROLLER_INACTIVITY_PROBE $OVN_GATEWAY_OPTIONS $OVNKUBE_NODE_MODE --metrics-bind-address 127.0.0.1:29103
+	--init-node ${K8S_NODE} --encap-ip ${SMART_NIC_IP} --nb-address $OVN_NB_DB_LIST --sb-address $OVN_SB_DB_LIST --nb-client-privkey /ovn-cert/tls.key --nb-client-cert /ovn-cert/tls.crt --nb-client-cacert /ovn-ca/ca-bundle.crt --nb-cert-common-name ovn --sb-client-privkey /ovn-cert/tls.key --sb-client-cert /ovn-cert/tls.crt --sb-client-cacert /ovn-ca/ca-bundle.crt --sb-cert-common-name ovn --config-file=/run/ovnkube-config/ovnkube.conf --loglevel ${OVN_KUBE_LOG_LEVEL}  --inactivity-probe=$OVN_CONTROLLER_INACTIVITY_PROBE $OVN_GATEWAY_OPTIONS $OVNKUBE_NODE_MODE $OVNKUBE_NODE_MGMT_PORT_NETDEV --metrics-bind-address 127.0.0.1:29103
